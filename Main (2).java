87e3ff9706cb8af3d34c7eac05fade8217d2bc97
evelynibarra07 <134562620+evelynibarra07@users.noreply.github.com>
2024-09-19 10:59:32 -0500

    Unit Conversion

---
+++ Main (2).java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      UnitConversion seconds = new MinutesToSeconds();
+      seconds.convert();
+      
+    }
+}
---
+++ UnitConversion.java
@@ -0,0 +1,5 @@
+interface UnitConversion {
+  void.convert();
+  
+
+}
