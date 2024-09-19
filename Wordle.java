99561f0ad57dbf716179d3947149f8a24638947d
ArianaAdame <132471158+ArianaAdame@users.noreply.github.com>
2024-09-19 10:58:20 -0500

    Create Wordle.java
    Interface for Wordle

---
+++ Wordle.java
@@ -0,0 +1,16 @@
+/**
+The Wordle interface
+@author Ariana Adame
+*/
+
+interface Wordle {
+    int TURNS = 5;
+    abstract String getWord();
+    abstract String getAnswer();
+    abstract void setWord(String newWord);
+    abstract void setAnswer(String newAnswer);
+    abstract String generateWord();
+    abstract boolean checkAnswer(String word);
+    abstract void newTurn();
+    abstract void endGame(boolean win);
+}
