b42842fec407f084d480ba2b243e9a066a6ad899
geode3 <132504158+geode3@users.noreply.github.com>
2024-09-19 10:59:53 -0500

    Create VMT.java

---
+++ VMT.java
@@ -0,0 +1,8 @@
+/**
+  This class is used to show a tour around a virtual museum contains the visitors information as well as the
+  times the museum is open.
+  @author Giovani Zuniga
+*/
+interface VMT {
+  public void ScheduleTour(); // interface method
+}
