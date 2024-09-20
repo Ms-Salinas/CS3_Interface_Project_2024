40f648bf50684f96c1dc9b8e1dbc7c21a85c365d
evelynibarra07 <134562620+evelynibarra07@users.noreply.github.com>
2024-09-20 10:59:09 -0500

    UnitConversion.java

--- UnitConversion.java
+++ UnitConversion.java
@@ -1,5 +1,11 @@
-interface UnitConversion {
-  void.convert();
-  
-
+/**
+ * This interface establishes methods that will be set in other classes, there should be three classes total [convertTime, convertTemp, and convertLength]
+ * 
+ * @author Evelyn Ibarra
+ */
+ 
+interface UnitConversion{
+    void convert(int x);
+    String 
+    
 }
