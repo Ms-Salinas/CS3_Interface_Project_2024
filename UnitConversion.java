670c121c2075803a57754983e284168ded4967d3
evelynibarra07 <134562620+evelynibarra07@users.noreply.github.com>
2024-09-23 10:13:27 -0500

    Update UnitConversion.java

--- UnitConversion.java
+++ UnitConversion.java
@@ -1,11 +1,35 @@
 /**
- * This interface establishes methods that will be set in other classes, there should be three classes total [convertTime, convertTemp, and convertLength]
- * 
- * @author Evelyn Ibarra
+  * This interface establishes methods that will be set in other classes, there should be three classes total [convertTime, convertTemp, and convertLength]
+  * 
+  * @author Evelyn Ibarra
  */
  
-interface UnitConversion{
-    void convert(int x);
-    String 
-    
+public interface UnitConversion{
+ /**
+  * Converts value parameter to a different unit using factor parameter.
+  *
+  * @param value of unconverted unit
+  * @param the amount used to convert value
+  * @return the amount converted 
+ */
+    double convert(double value, double factor);
+ /**
+  * Sets factor of conversion.
+  *
+  * @param value to use when converting from one unit to another.
+ */
+    double setConversionFactor(double factor);
+ /**
+  * gets conversion factor
+ */
+    double getConversionFactor();
+ /**
+  *Resets conversion factor. 
+ */
+    void resetConversionFactor();
+  /**
+  * Shows the completed conversion.  
+ */
+    void showConversion();
+
 }
