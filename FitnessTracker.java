5d5ee01b57183d753ad6bf618d1159273acfcc17
raid4lime <132471034+raid4lime@users.noreply.github.com>
2024-09-26 10:56:08 -0500

    FitnessTracker.java

---
+++ FitnessTracker.java
@@ -0,0 +1,11 @@
+interface FitnessTracker{
+
+/** @author Jacob Dovalina
+    @implementer Casey Martinez*/
+
+    // Method used to calculate avg calories burned
+    double caloriesBurned();
+    // Method used to store amount of time doing the activty
+    int amountOfTime();
+    
+}
